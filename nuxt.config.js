54fadfd1d7d31646f90c849ca442a382f9fbfeee
SideeID <sandipratama164026@gmail.com>
2024-04-19 20:05:14 +0700

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,4 @@
-// export default {
-//   // Target: https://go.nuxtjs.dev/config-target
-//   target: 'static',
-
-//   // Global page headers: https://go.nuxtjs.dev/config-head
-//   head: {
-//     title: 'Side ID',
-//     htmlAttrs: {
-//       lang: 'en',
-//     },
-//     meta: [
-//       { charset: 'utf-8' },
-//       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-//       { name: 'format-detection', content: 'telephone=no' },
-//       {
-//         name: 'description',
-//         content:
-//           "Saya seorang manusia yang suka melakukan kegiatan yang tidak biasa, dan suka dengan anime. Selain ingin menikah dengan anime saya juga ingin menjadi seorang fullstack developer.",
-//       },
-//       {
-//         name: 'og:description',
-//         content:
-//           "Saya seorang manusia yang suka melakukan kegiatan yang tidak biasa, dan suka dengan anime. Selain ingin menikah dengan anime saya juga ingin menjadi seorang fullstack developer.",
-//       },
-//       { name: 'og:title', content: 'Side ID [he/they]' },
-//       { name: 'og:image', content: '/favicon.png' },
-//     ],
-//     link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
-//   },
-
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
@@ -44,10 +11,21 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'description',
+        content:
+          "Saya seorang manusia yang suka melakukan kegiatan yang tidak biasa, dan suka dengan anime. Selain ingin menikah dengan anime saya juga ingin menjadi seorang fullstack developer.",
+      },
+      {
+        name: 'og:description',
+        content:
+          "Saya seorang manusia yang suka melakukan kegiatan yang tidak biasa, dan suka dengan anime. Selain ingin menikah dengan anime saya juga ingin menjadi seorang fullstack developer.",
+      },
+      { name: 'og:title', content: 'Side ID [he/they]' },
+      { name: 'og:image', content: '/favicon.png' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
