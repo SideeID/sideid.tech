a07efa952088431003ba563d5b7ef9cf8702a84e
SideeID <sandipratama164026@gmail.com>
2024-05-24 10:29:52 +0700

    chore: Rearrange CSS class order in Navbar.vue for consistency

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="my-4 mx-5">
+  <div class="mx-5 my-4">
     <div class="flex flex-row">
       <div class="select-none">
         <h1 class="flex items-center">
           <img
             src="~/assets/img/avatar.png"
-            class="w-10 h-10 rounded-lg inline avatar"
+            class="inline w-10 h-10 rounded-lg avatar"
             @click="toggleFurry"
           />
-          <span class="ml-2 font-bold text-lg">SideID</span>
+          <span class="ml-2 text-lg font-bold">SideID</span>
         </h1>
       </div>
-
-
 
       <div class="ml-auto">
         <ThemeSwitcher />
@@ -29,12 +27,12 @@ export default {
       for (const a of avatars) {
         a.src = a.src.includes('redpanda')
           ? require('~/assets/img/avatar.png')
-          : '/redpanda.png'
+          : '/redpanda.jpg'
       }
 
       const favicon = document.querySelectorAll('[rel="icon"]')[0]
       favicon.href = favicon.href.includes('favicon')
-        ? '/redpanda.png'
+        ? '/redpanda.jpg'
         : 'favicon.png'
     },
   },
