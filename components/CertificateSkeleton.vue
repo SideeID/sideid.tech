103fab53474fa09fe07072657177ef1876cb2608
SideeID <sandipratama164026@gmail.com>
2025-01-17 12:15:43 +0700

    feat: Add CertificateSkeleton component for loading state representation

---
+++ components/CertificateSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
+    <div class="w-full h-48 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
+    <div class="p-4">
+      <div class="h-6 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2 animate-pulse"></div>
+      <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2 mb-2 animate-pulse"></div>
+      <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/4 animate-pulse"></div>
+    </div>
+  </div>
+</template>
