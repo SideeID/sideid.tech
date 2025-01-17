e05b8cdbd11c8355eeeb8ec0a553ddaf011f5505
SideeID <sandipratama164026@gmail.com>
2025-01-17 12:15:47 +0700

    style: Enhance Project component styles for better visibility and responsiveness

--- components/Project.vue
+++ components/Project.vue
@@ -2,8 +2,8 @@
   <a
     :href="link"
     target="_blank"
-    class="flex-1 p-4 my-2 duration-300 ease-in-out transform rounded-md bg-opacity-30 hover:scale-105"
-    :class="color"
+    class="flex-1 p-4 my-2 duration-300 ease-in-out transform rounded-md bg-opacity-30 hover:scale-105 hover:shadow-lg transition-all"
+    :class="[color, { 'dark:bg-opacity-20': isDarkMode }]"
   >
     <div class="flex flex-row items-center">
       <img
@@ -20,11 +20,11 @@
       <div class="flex flex-col justify-center ml-4">
         <h2 class="flex items-center text-lg font-bold">
           {{ name }}
-          <span v-if="year" class="hidden ml-4 text-sm font-normal sm:block"
+          <span v-if="year" class="hidden ml-4 text-sm font-normal sm:block text-gray-600 dark:text-gray-400"
             >[{{ year }}]</span
           >
         </h2>
-        <p class="text-sm">
+        <p class="text-sm text-gray-700 dark:text-gray-300">
           {{ description }}
         </p>
       </div>
